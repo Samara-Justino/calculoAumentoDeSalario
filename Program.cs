f0555150aa42ab6d9d236c0a83066e8aa299726b
Samara Justino <samarasouzajustino@gmail.com>
2024-09-28 17:27:15 -0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,7 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.Write("Qual o salário do funcionário? ");
+double salario = double.Parse(Console.ReadLine());
+Console.Write("Qual a porcentagem para o reajuste? ");
+double porcentagem = double.Parse(Console.ReadLine());
+double novoSalario = salario + (salario * (porcentagem / 100));
+Console.WriteLine("Novo salário é R$ " + novoSalario.ToString("F2"));
